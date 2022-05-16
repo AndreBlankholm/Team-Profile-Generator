0b06b16afd8b3c33b1e2791d18f0c60209e08500
AndreBlankholm <ablankholm@gmail.com>
2022-05-16 12:47:45 -0500

    testing and color changes

--- _test_/Manager.test.js
+++ _test_/Manager.test.js
@@ -1,1 +1,12 @@
 const Manager = require('../lib/Manager');
+// Creates manager object
+test('creates an Manager object', () => {
+    const manager = new Manager('Andre', 1, 'ablankholm@gmail.com', 2);
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+// Gets getRole()
+test('gets role of employee', () => {
+    const manager = new Manager('Andre', 1, 'ablankholm@gmail.com');
+    expect(manager.getRole()).toEqual("Manager");
+});
